ea225b9d064efc64a1ed5818ff088a2069bd620f
scy <sauchyer@gmail.com>
2025-01-17 00:18:15 +0800

    add pro

---
+++ GuozaokeTests/GuozaokeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuozaokeTests.swift
+//  GuozaokeTests
+//
+//  Created by scy on 2025/1/12.
+//
+
+import Testing
+@testable import Guozaoke
+
+struct GuozaokeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
